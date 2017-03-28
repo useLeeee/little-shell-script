b40047d1010894a77c8665074d04660d8d6bb1b1
useLeeee <userLeeee@gmail.com>
2017-03-29 00:49:43 +0800

    Rename pm2.5DataPrintf.sh to pm25DataPrintf.sh

--- pm2.5DataPrintf.sh
+++
@@ -1,2 +0,0 @@
-str=`awk '/数据更新时间/' *.html | head -1 | awk '{print $2,$3}' | sed -e 's/数据更新时间：//g' -e $'s/\'//g' -e 's/,/ /g'`
-awk '/td|tr/{print $1}' *.html | awk 'BEGIN{FS="\n";RS="tr>";}{print $2,$6}' | awk 'gsub("<td>|</td>","")'| awk '{$1="'"$str"'" $1;print}'
